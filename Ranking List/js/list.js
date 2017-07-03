6cb460918aa4d8b0bc0f1fecef2931701b70ea86
yanhua1117 <yh_rw828@163.com>
2017-07-03 19:51:59 +0800

    test

---
+++ Ranking List/js/list.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var oList = document.getElementById('list');
+	var aLi = oList.getElementsByTagName('li');
+	var aImg = ['img/cake01,jpg','img/cake02,jpg','img/cake03,jpg','img/cake04,jpg','img/cake05,jpg','img/cake01,jpg','img/cake02,jpg','img/cake03,jpg','img/cake04,jpg','img/cake05,jpg'];
+	var aP = 
+}
