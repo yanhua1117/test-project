81f6c3a8318fb5f5b265aeb273352a093ec2d22f
yanhua1117 <yh_rw828@163.com>
2017-08-25 18:33:03 +0800

    0825

---
+++ blog/js/main.js
@@ -0,0 +1,9 @@
+//$(function(){
+////	var menu_link = document.getElementById('a1');
+//	$('#a1').click(function () {
+//	$('html, body').animate({
+//	scrollTop: $($.attr(this, 'href')).offset().top
+//	}, 500);
+//	return false;
+//	});
+//})
