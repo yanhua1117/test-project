9f1600439b8e289e3b41190c497d65df0d648f40
yanhua1117 <yh_rw828@163.com>
2017-09-04 15:25:14 +0800

    0904

--- blog/js/main.js
+++ blog/js/main.js
@@ -19,5 +19,59 @@ window.onload = function(){
 		}
 	}
 	
+	//当点击关闭按钮时，菜单列表显示或隐藏
+	var menuIcon = document.getElementsByClassName('menu-2_icon')[0];
+	var menuList = document.getElementsByClassName('menu-2_list')[0];
+	var onOff=true;
+	menuIcon.onclick = function(){
+		if (onOff) {
+			menuList.style.display = 'block';
+			onOff = false;
+		} else{
+			menuList.style.display = 'none';
+			onOff = true;
+		}
+
+	}
+	
+	//当点击菜单中的选项时，菜单隐藏
+	var menuSm = document.getElementById('menu-item');
+	var menuSm_list = menuSm.getElementsByTagName('a');
+	
+	for ( var i = 0; i < menuSm_list.length; i++ ) {
+		
+		menuSm_list[i].onclick = function(){
+			
+			if (!onOff) {
+				onOff = true;
+			}
+			menuList.style.display = 'none';
+			
+		}
+		
+	}
+	
+	//点击联系方式按钮切换二维码
+	var contactIcon = document.getElementsByClassName('personal-icon');
+	var closeIcon = document.getElementById('close-icon');
+	var contactImg = document.getElementById('contact-img');
+//	var contact = document.getElementsByClassName('contact')[0];
+	contactIcon[1].onclick = function(){
+		
+		$('.contact').addClass('active-contact');
+		contactImg.src = 'img/wechat.jpg';
+		
+	}
+	contactIcon[2].onclick = function(){
+		
+		$('.contact').addClass('active-contact');
+		contactImg.src = 'img/weibo.jpg';
+		
+	}
+	closeIcon.onclick = function(){
+		
+		$('.contact').removeClass('active-contact');
+		
+	}
 	
 }
